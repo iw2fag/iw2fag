f9adfb17dc80893539a1d880f6a0ca4251de5386
henry <han-xiang.yang@hpe.com>
2017-12-18 11:25:34 +0800

    add SelectSort

--- core-java/src/main/java/com/iw2fag/lab/algorithm/sort/SelectSort.java
+++ core-java/src/main/java/com/iw2fag/lab/algorithm/sort/SelectSort.java
@@ -7,4 +7,24 @@ package com.iw2fag.lab.algorithm.sort;
  * Time: 11:13 AM
  */
 public class SelectSort {
+
+
+    public static int[] selectSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            int j;
+            for (j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        return arr;
+
+    }
+
 }
