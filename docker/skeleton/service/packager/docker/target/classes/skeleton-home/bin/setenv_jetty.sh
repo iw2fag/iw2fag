2bd069ecd23c70d2632d0d18cddae26c877c84b0
henry <han-xiang.yang@hpe.com>
2018-03-14 13:45:28 +0800

    fixed untracked files

--- docker/skeleton/service/packager/docker/target/classes/skeleton-home/bin/setenv_jetty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export JETTY_LOGS=$SKELETON_HOME/log
-
-export JETTY_STATE=$JETTY_HOME/jetty.state
-
-export JAVA_OPTIONS="$JAVA_OPTIONS -Djetty.home=$JETTY_HOME -Djetty.base=$JETTY_BASE -Djetty.state=$JETTY_STATE -Djetty.logs=$JETTY_LOGS -Dpath=$ADD_TO_CLASSPATH"
-
